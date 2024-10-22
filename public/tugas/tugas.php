e5bef99aee17ad327d8eeb4efa1e8f42d9901cb8
bagussatoto <bagusbudisatoto2001@gmail.com>
2024-10-22 13:49:08 +0700

    update

--- public/tugas/tugas.php
+++ public/tugas/tugas.php
@@ -67,6 +67,11 @@ echo "Number of rows: " . $result->num_rows . "<br>";
         <?php else: ?>
             <p class="text-gray-600">Belum ada tugas.</p>
         <?php endif; ?>
+        <tr>
+                <td colspan="4" class="px-6 py-4 whitespace-nowrap text-center">
+                    <a href="../role.php" role="button" class="mr-6 inline-block rounded bg-blue-500 text-white shadow-[0_4px_9px_-4px_rgba(51,45,45,0.7)] hover:bg-blue-600 hover:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:bg-blue-800 focus:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] active:bg-blue-700 active:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal transition duration-150 ease-in-out focus:outline-none focus:ring-0">kembali</a>
+                </td>
+            </tr>
     </div>
 </body>
 </html>
