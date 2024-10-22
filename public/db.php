96dd764ff1ecd64abf04f3bf726bb619f973b711
bagussatoto <bagusbudisatoto2001@gmail.com>
2024-10-22 12:07:18 +0700

    database

--- public/db.php
+++ public/db.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$database = "smansagong";
+$database = "learning";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database);
